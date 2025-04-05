05b395b76def3d8028817d3236a435da35dd691e
Dee-Dee-R <david.dellarossa@gmail.com>
2025-04-05 15:54:14 +0200

    Added MessageBroker to project

---
+++ Assets/Scripts/MessageBroker/Editor/MessageBrokerGenerator.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace MessageBroker.Editor
+{
+    public static partial class MessageBrokerGenerator
+    {
+        private static readonly string PackageName = "com.deedeer.messagebroker";
+        private static readonly string ClassName = "MessageBroker";
+        private static string _packageVersion = String.Empty;
+        private static string _namespace;
+        private static string _outputFolder;
+        private static string _categoryPrefix;
+        private static string _defaultCategoryName;
+        
+        [MenuItem("DeeDeeR/MessageBroker/Generate Message Broker")]
+        static async Task GenerateMessageBroker()
+        {
+            _packageVersion = GetPackageVersion();
+
+            var userInput = await MessageBrokerGeneratorUI.ShowDialog();
+            
+            if (userInput == null)
+            {
+                Debug.Log("Generation canceled.");
+                return;
+            }
+            
+            _namespace = userInput.Namespace;
+            _outputFolder = userInput.OutputFolder;
+            _categoryPrefix = userInput.CategoryPrefix;
+            _defaultCategoryName = userInput.DefaultCategoryName;
+            
+            var messageInfos = GetAllMessages().ToArray();
+
+            try
+            {
+                AddDefaultInputParametersToMessages(messageInfos);
+
+                var messageBrokerCategoryGenerator = new MessageBrokerCategoryGenerator(messageInfos);
+                messageBrokerCategoryGenerator.Generate();
+
+                var messageBrokerInterfaceGenerator = new MessageBrokerGenerator.MessageBrokerInterfaceGenerator(messageInfos);
+                messageBrokerInterfaceGenerator.Generate();
+
+                var messageBrokerCommonGenerator = new MessageBrokerCommonGenerator(messageInfos);
+                messageBrokerCommonGenerator.Generate();
+
+            }
+            finally
+            {
+                RemoveDefaultInputParametersToMessages(messageInfos);
+            }
+        }
+
+        private static void RemoveDefaultInputParametersToMessages(IEnumerable<MessageInfo> messageInfos)
+        {
+            foreach (var messageInfo in messageInfos)
+            {
+                messageInfo.Message.InputParameters.RemoveAt(0);
+                messageInfo.Message.InputParameters.RemoveAt(0);
+            }
+        }
+
+        private static void AddDefaultInputParametersToMessages(IEnumerable<MessageInfo> messageInfos)
+        {
+            foreach(var messageInfo in messageInfos)
+            {
+                messageInfo.Message.InputParameters.Insert(0,
+                    new InputParameter()
+                    {
+                        multiplicity = Multiplicity.Single,
+                        parameterName = "target",
+                        parameterType = ParameterType.ObjectType,
+                        parameterComment = "The target of the message. Optional."
+                    });
+
+                messageInfo.Message.InputParameters.Insert(0,
+                    new InputParameter()
+                    {
+                        multiplicity = Multiplicity.Single,
+                        parameterName = "sender",
+                        parameterType = ParameterType.ObjectType,
+                        parameterComment = "The sender of the message. Required."
+                    }
+                    );
+            }
+        }
+
+        private static string GetPackageVersion()
+        {
+            var packages = UnityEditor.PackageManager.PackageInfo.GetAllRegisteredPackages();
+            var package = packages.SingleOrDefault(x => x.name == PackageName);
+            if (package is not null)
+            {
+                return package.version;
+            }
+            else
+            {
+                return "<version undefined>";
+            }
+        }
+
+        private static IEnumerable<MessageInfo> GetAllMessages()
+        {
+            var currentScriptGuid = AssetDatabase.FindAssets($"t:Script {nameof(global::MessageBroker.Editor.MessageBrokerGenerator)}");
+            var currentScriptLocation = AssetDatabase.GUIDToAssetPath(currentScriptGuid[0]);
+            var currentFolder = currentScriptLocation.Substring(0, currentScriptLocation.LastIndexOf('/'));
+
+            string[] guids = AssetDatabase.FindAssets("t:" + nameof(Message), new[] { currentFolder });
+            var messageInfos = new List<MessageInfo>();
+            for (int i = 0; i < guids.Length; i++) 
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                messageInfos.Add(new MessageInfo()
+                    {
+                        Message = AssetDatabase.LoadAssetAtPath<Message>(path),
+                        Path = path
+                    });
+            }
+            return messageInfos.ToArray();
+        }
+
+        private static string CleanName(string name)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(name, @"[^a-zA-Z0-9_]", string.Empty);
+        }
+
+    }
+    internal class MessageInfo
+    {
+        public Message Message;
+        public string Path;
+    }
+}
---
+++ Assets/Scripts/MessageBroker/Editor/MessageBrokerMessagesInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DnD.Code.Scripts.Helpers;
+using UnityEditor;
+using UnityEngine;
+
+namespace MessageBroker.Editor
+{
+    public static partial class MessageBrokerMessagesInitializer
+    {
+        public static readonly string MessageBrokerMessagesPath = "Assets/Scripts/MessageBroker/Editor/Messages";
+        
+        [MenuItem("DeeDeeR/MessageBroker/Generate Messages")]
+        public static void InitializeMessages()
+        {
+            Debug.Log("Initializing MessageBroker Messages");
+
+            try
+            {
+                FileSystemHelper.EnsureFolderExists(MessageBrokerMessagesPath, true);
+                
+                GameMessagesInitializer.InitializeGameMessages();
+                MenusMessagesInitializer.InitializeMenusMessages();
+
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+                        
+            Debug.Log("MessageBroker Messages initialization completed");
+        }
+    }
+}
