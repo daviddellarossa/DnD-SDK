2e792f823d34302e42943ddd3e1f8e48336b5976
Dee-Dee-R <david.dellarossa@gmail.com>
2025-04-06 08:36:56 +0200

    More on Character Build UI

--- Assets/Scripts/Management/CharacterBuild/CharacterBuildSceneManager.cs
+++ Assets/Scripts/Management/CharacterBuild/CharacterBuildSceneManager.cs
@@ -1,9 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.UIElements;
 
 namespace Management.CharacterBuild
 {
     public class CharacterBuildSceneManager : MonoBehaviour
     {
+        public CharacterBuildSceneManagerCore Core { get; protected set; }
+        
+        private UIDocument uiDocument;
+        
+        void Awake()
+        {
+            Core = new CharacterBuildSceneManagerCore(this);
+
+            Core.OnAwake();
+            
+            uiDocument = gameObject.GetComponent<UIDocument>();
+
+            if (uiDocument == null)
+            {
+                Debug.LogError("UI Document is null");
+            }
+            
+            var root = uiDocument.rootVisualElement;
+            
+            // Set localized text for Character Name
+            var txtCharacterName = root.Q<TextField>("txtCharacterName");
+            var txtCharacterNameLabel = new LocalizedString("CharacterUI", "UI.CharacterBuild.CharacterName");
+            txtCharacterNameLabel.StringChanged += (localizedText) => txtCharacterName.label = localizedText;
+            
+            // Set localized text for Character Name
+            var ddfSpecies = root.Q<DropdownField>("ddfSpecies");
+            var ddfSpeciesLabel = new LocalizedString("CharacterUI", "UI.CharacterBuild.Species");
+            ddfSpeciesLabel.StringChanged += (localizedText) => ddfSpecies.label = localizedText;
+            
+            //Dvar species = DnD.Helpers.
+        }
         
     }
 }
