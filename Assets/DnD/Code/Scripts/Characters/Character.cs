d55006375eecae64f25b9eb2959e7bfac2d4c1b3
Dee-Dee-R <david.dellarossa@gmail.com>
2025-04-23 07:03:25 +0200

    Add Player object to Test scene

--- Assets/DnD/Code/Scripts/Characters/Character.cs
+++ Assets/DnD/Code/Scripts/Characters/Character.cs
@@ -5,12 +5,23 @@ namespace DnD.Code.Scripts.Characters
     public class Character : MonoBehaviour
     {
         [SerializeField]
-        private CharacterStats CharacterStats;
+        private CharacterStats characterStats;
+
+        public CharacterStats CharacterStats
+        {
+            get => characterStats;
+            set => characterStats = value;
+        }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            Debug.Log("Character start");
+        }
         
+        void Awake()
+        {
+            Debug.Log("Character awoken");
         }
 
         // Update is called once per frame
--- Assets/Scripts/Management/Levels/LevelManager.cs
+++ Assets/Scripts/Management/Levels/LevelManager.cs
@@ -1,13 +1,46 @@
-﻿using Infrastructure.SaveManager;
+﻿using System.Linq;
+using DnD.Code.Scripts.Characters;
+using Infrastructure.SaveManager;
 using Management.Scene;
+using UnityEngine;
 
 namespace Management.Levels
 {
     public class LevelManager : SceneManager
     {
+        private SaveGameData saveGameData;
         public void SetSaveGameData(SaveGameData data)
         {
+            this.saveGameData = data;
+
+        }
+        void Start()
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+            if (playerObject is null)
+            {
+                Debug.LogError("PlayerCharacter GameObject not found.");
+                return;
+            }
             
+            var character = playerObject.GetComponent<Character>();
+            
+            if (character is null)
+            {
+                Debug.LogError("Player not found");
+                return;
+            }
+            
+            var saveGameDataToEntityConverter = new SaveGameDataToEntityConverter();
+            var characterStats = saveGameDataToEntityConverter.Convert(saveGameData.CharacterStats);
+
+            if (characterStats is null)
+            {
+                Debug.LogError("Unable to extract characterStats from saveGameData");
+                return;
+            }
+            character.CharacterStats = characterStats;
         }
     }
 }
