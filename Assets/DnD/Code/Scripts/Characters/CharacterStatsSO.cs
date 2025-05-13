8ea3cda2e6222c3b969db0e53554a10d80e953c3
Dee-Dee-R <david.dellarossa@gmail.com>
2025-05-13 21:28:17 +0200

    Added Character stats to companions

---
+++ Assets/DnD/Code/Scripts/Characters/CharacterStatsSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DnD.Code.Scripts.Characters
+{
+    [CreateAssetMenu(fileName = "NewCharacterStat", menuName = "Game Entities/Character/Character Stats")]
+    public class CharacterStatsSo: ScriptableObject
+    {
+        [field: SerializeField]
+        public CharacterStats CharacterStats { get; set; }
+    }
+}
